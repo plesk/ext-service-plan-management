e0410802a7618b176d07dacc68b8063e368347cf
snoskova07 <snoskova07@gmail.com>
2016-06-09 13:59:48 +0600

    Add hasPermission function check

---
+++ plib/library/Form/CreateForm.php
@@ -0,0 +1,26 @@
+<?php
+// Copyright 1999-2016. Parallels IP Holdings GmbH.
+class Modules_ServicePlanManagement_Form_CreateForm extends pm_Form_Simple
+{
+    protected $_clientId;
+    protected $_domainId;
+
+    public function init()
+    {
+        $this->addElement('Text', 'clientId', [
+            'label' => 'Client/Reseller ID: ',
+            'value' => $this->_clientId,
+        ]);
+
+        $this->addElement('Text', 'domainId', [
+            'label' => 'Domain ID: ',
+            'value' => $this->_domainId,
+        ]);
+
+        $this->addControlButtons([
+            'sendTitle' => "OK",
+            'cancelTitle' => "Cancel",
+            'cancelLink' => pm_Context::getModulesListUrl(),
+        ]);
+    }
+}
