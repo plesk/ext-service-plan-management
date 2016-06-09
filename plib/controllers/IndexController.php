30d7c603ad4d967ee49a2bf6eb0921c0bbe96005
snoskova07 <snoskova07@gmail.com>
2016-06-09 14:03:43 +0600

    Update IndexController.php

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -25,7 +25,33 @@ class IndexController extends pm_Controller_Action
         $this->view->fakeSitesLimit = $this->_domain->getLimit('max_fake_sites');
     }
 
-    public function indexAction()
+        public function indexAction()
     {
+        //Check that client/reseller has permission 'fake_sites' on subscription or not.
+        $form = new Modules_ServicePlanManagement_Form_CreateForm();
+        if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
+            try {
+                $values = $form->getValues();
+
+                $clientId = $values['clientId'];
+                $domainId = $values['domainId'];
+                $domain = new pm_Domain($domainId);
+                $client = pm_Client::getByClientId($clientId);
+                $result = $client->hasPermission('fake_sites', $domain);
+
+                $text = (bool)$result
+                    ? "Client with login '{$client->getProperty('login')}' (ID {$client->getId()}) has permission 'fake_sites' on domain '{$domain->getName()}'."
+                    : "Client with login '{$client->getProperty('login')}' (ID {$client->getId()}) has no permission 'fake_sites' on domain '{$domain->getName()}'.";
+
+                $this->_status->addInfo($text);
+                pm_Log::info('RESULT: ' . (bool)$result);
+                $this->redirect(pm_Context::getActionUrl('index', 'index'));
+
+            }   catch (pm_Exception $exception) {
+                $this->_status->addError($exception->getMessage());
+                $this->redirect(pm_Context::getActionUrl('index', 'index'));
+            }
+        }
+        $this->view->form = $form;
     }
 }
